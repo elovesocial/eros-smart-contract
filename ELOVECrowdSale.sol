8ef52c11e25b4867d144ac3950f2280ad2ce08e3
Tien Luong NGUYEN <luongnt@nexttech.asia>
2018-02-13 11:57:10 +0700

    remove string lib

--- ELOVECrowdSale.sol
+++ ELOVECrowdSale.sol
@@ -32,8 +32,6 @@ contract ApproveAndCallFallBack {
 // Owned contract
 contract Owned {
     
-    using Strings for string;
-    
     struct Investor {
         address sender;
         uint amount;
@@ -41,10 +39,11 @@ contract Owned {
     }
     
     // version of this smart contract
-    string public version = "1.4";
+    string public version = "1.0";
     
     address public owner;
     address public newOwner;
+    // reward pool wallet, un-sold tokens will be burned to this address
     address public rewardPoolWallet;
     
     // List of investors with invested amount in ETH
@@ -69,17 +68,6 @@ contract Owned {
         require(kycStatus == !investors[mapInvestors[inv]-1].kyced);
         investors[mapInvestors[inv]-1].kyced = kycStatus;
         return true;
-    }
-    
-    // Give KYC status in batch
-    // investor separated by ';'
-    function changeKYCStatusInBatch(string invs, bool kycStatus) onlyOwner public {
-        string[] storage listInvestors = invs.split(";");
-        for(uint i = 0; i<listInvestors.length; i++) {
-            if (mapInvestors[listInvestors[i].parseAddr()]>0) {
-                investors[mapInvestors[listInvestors[i].parseAddr()]-1].kyced = kycStatus;
-            }
-        }
     }
     
     function setRewardPoolWallet(address rewardWallet) onlyOwner public returns(bool success) {
@@ -235,8 +223,8 @@ contract ELOVEToken is ERC20Interface, Owned {
         require(icoEnded);
         // transaction is in tradable period
         require(now<tokenLockTime);
-        // sender is not founder, and must be kyc-ed
-        require(!founders[msg.sender] && investors[mapInvestors[msg.sender]-1].kyced);
+        // sender is not founder, and must be kyc-ed or is RewardPoolWallet
+        require(!founders[msg.sender] && (msg.sender == rewardPoolWallet || investors[mapInvestors[msg.sender]-1].kyced));
         // sender either is owner or recipient is not 0x0 address
         require(msg.sender == owner || to != 0x0);
         
@@ -319,7 +307,6 @@ contract ELOVEToken is ERC20Interface, Owned {
                 mapInvestors[msg.sender] = ind;
             }
         } else {
-            //var neededEtherToBuy = (10**18*roundTokenLeft[round]*(100-roundDiscount[round])).div(10**uint(decimals)*etherExRate*100);
             var neededEtherToBuy = (10**18*roundTokenLeft[round]*100).div(10**uint(decimals)).div(etherExRate*(100+roundBonus[round]));
             balances[owner] = balances[owner] - roundTokenLeft[round];
             balances[msg.sender] = balances[msg.sender] + roundTokenLeft[round];
@@ -357,7 +344,6 @@ contract ELOVEToken is ERC20Interface, Owned {
             processRound(3);
         } else {
             // crowdsale ends, check success conditions
-            // run once
             if (this.balance<softcap) {
                 // time to send back funds to investors
                 for(uint i = 0; i<investors.length; i++) {
@@ -412,236 +398,5 @@ library SafeMath {
         require(b > 0);
         c = a / b;
     }
-}
-
-library Strings {
-
-    /**
-     * Concat (High gas cost)
-     * 
-     * Appends two strings together and returns a new value
-     * 
-     * @param _base When being used for a data type this is the extended object
-     *              otherwise this is the string which will be the concatenated
-     *              prefix
-     * @param _value The value to be the concatenated suffix
-     * @return string The resulting string from combinging the base and value
-     */
-    function concat(string _base, string _value) pure internal returns (string) {
-        bytes memory _baseBytes = bytes(_base);
-        bytes memory _valueBytes = bytes(_value);
-
-        assert(_valueBytes.length > 0);
-
-        string memory _tmpValue = new string(_baseBytes.length + 
-            _valueBytes.length);
-        bytes memory _newValue = bytes(_tmpValue);
-
-        uint i;
-        uint j;
-
-        for(i = 0; i < _baseBytes.length; i++) {
-            _newValue[j++] = _baseBytes[i];
-        }
-
-        for(i = 0; i<_valueBytes.length; i++) {
-            _newValue[j++] = _valueBytes[i];
-        }
-
-        return string(_newValue);
-    }
-
-    /**
-     * Index Of
-     *
-     * Locates and returns the position of a character within a string
-     * 
-     * @param _base When being used for a data type this is the extended object
-     *              otherwise this is the string acting as the haystack to be
-     *              searched
-     * @param _value The needle to search for, at present this is currently
-     *               limited to one character
-     * @return int The position of the needle starting from 0 and returning -1
-     *             in the case of no matches found
-     */
-    function indexOf(string _base, string _value) pure internal returns (int) {
-        return _indexOf(_base, _value, 0);
-    }
-
-    /**
-     * Index Of
-     *
-     * Locates and returns the position of a character within a string starting
-     * from a defined offset
-     * 
-     * @param _base When being used for a data type this is the extended object
-     *              otherwise this is the string acting as the haystack to be
-     *              searched
-     * @param _value The needle to search for, at present this is currently
-     *               limited to one character
-     * @param _offset The starting point to start searching from which can start
-     *                from 0, but must not exceed the length of the string
-     * @return int The position of the needle starting from 0 and returning -1
-     *             in the case of no matches found
-     */
-    function _indexOf(string _base, string _value, uint _offset) pure internal returns (int) {
-        bytes memory _baseBytes = bytes(_base);
-        bytes memory _valueBytes = bytes(_value);
-
-        assert(_valueBytes.length == 1);
-
-        for(uint i = _offset; i < _baseBytes.length; i++) {
-            if (_baseBytes[i] == _valueBytes[0]) {
-                return int(i);
-            }
-        }
-
-        return -1;
-    }
-
-    /**
-     * Length
-     * 
-     * Returns the length of the specified string
-     * 
-     * @param _base When being used for a data type this is the extended object
-     *              otherwise this is the string to be measured
-     * @return uint The length of the passed string
-     */
-    function length(string _base) pure internal returns (uint) {
-        bytes memory _baseBytes = bytes(_base);
-        return _baseBytes.length;
-    }
-
-    /**
-     * Sub String
-     * 
-     * Extracts the beginning part of a string based on the desired length
-     * 
-     * @param _base When being used for a data type this is the extended object
-     *              otherwise this is the string that will be used for 
-     *              extracting the sub string from
-     * @param _length The length of the sub string to be extracted from the base
-     * @return string The extracted sub string
-     */
-    function substring(string _base, int _length) pure internal returns (string) {
-        return _substring(_base, _length, 0);
-    }
-
-    /**
-     * Sub String
-     * 
-     * Extracts the part of a string based on the desired length and offset. The
-     * offset and length must not exceed the lenth of the base string.
-     * 
-     * @param _base When being used for a data type this is the extended object
-     *              otherwise this is the string that will be used for 
-     *              extracting the sub string from
-     * @param _length The length of the sub string to be extracted from the base
-     * @param _offset The starting point to extract the sub string from
-     * @return string The extracted sub string
-     */
-    function _substring(string _base, int _length, int _offset) pure internal returns (string) {
-        bytes memory _baseBytes = bytes(_base);
-
-        assert(uint(_offset+_length) <= _baseBytes.length);
-
-        string memory _tmp = new string(uint(_length));
-        bytes memory _tmpBytes = bytes(_tmp);
-
-        uint j = 0;
-        for(uint i = uint(_offset); i < uint(_offset+_length); i++) {
-          _tmpBytes[j++] = _baseBytes[i];
-        }
-
-        return string(_tmpBytes);
-    }
-
-    /**
-     * String Split (Very high gas cost)
-     *
-     * Splits a string into an array of strings based off the delimiter value.
-     * Please note this can be quite a gas expensive function due to the use of
-     * storage so only use if really required.
-     *
-     * @param _base When being used for a data type this is the extended object
-     *               otherwise this is the string value to be split.
-     * @param _value The delimiter to split the string on which must be a single
-     *               character
-     * @return string[] An array of values split based off the delimiter, but
-     *                  do not container the delimiter.
-     */
-    function split(string _base, string _value)
-        internal
-        returns (string[] storage splitArr) {
-        bytes memory _baseBytes = bytes(_base);
-        uint _offset = 0;
-
-        while(_offset < _baseBytes.length-1) {
-
-            int _limit = _indexOf(_base, _value, _offset);
-            if (_limit == -1) {
-                _limit = int(_baseBytes.length);
-            }
-
-            string memory _tmp = new string(uint(_limit)-_offset);
-            bytes memory _tmpBytes = bytes(_tmp);
-
-            uint j = 0;
-            for(uint i = _offset; i < uint(_limit); i++) {
-                _tmpBytes[j++] = _baseBytes[i];
-            }
-            _offset = uint(_limit) + 1;
-            splitArr.push(string(_tmpBytes));
-        }
-        return splitArr;
-    }
-
-    /**
-     * Compare To
-     * 
-     * Compares the characters of two strings, to ensure that they have an 
-     * identical footprint
-     * 
-     * @param _base When being used for a data type this is the extended object
-     *               otherwise this is the string base to compare against
-     * @param _value The string the base is being compared to
-     * @return bool Simply notates if the two string have an equivalent
-     */
-    function compareTo(string _base, string _value) pure internal returns (bool) {
-        bytes memory _baseBytes = bytes(_base);
-        bytes memory _valueBytes = bytes(_value);
-
-        if (_baseBytes.length != _valueBytes.length) {
-            return false;
-        }
-
-        for(uint i = 0; i < _baseBytes.length; i++) {
-            if (_baseBytes[i] != _valueBytes[i]) {
-                return false;
-            }
-        }
-
-        return true;
-    }
-    
-    function parseAddr(string _a) pure internal returns (address){
-        bytes memory tmp = bytes(_a);
-        uint160 iaddr = 0;
-        uint160 b1;
-        uint160 b2;
-        for (uint i=2; i<2+2*20; i+=2){
-            iaddr *= 256;
-            b1 = uint160(tmp[i]);
-            b2 = uint160(tmp[i+1]);
-            if ((b1 >= 97)&&(b1 <= 102)) b1 -= 87;
-            else if ((b1 >= 48)&&(b1 <= 57)) b1 -= 48;
-            if ((b2 >= 97)&&(b2 <= 102)) b2 -= 87;
-            else if ((b2 >= 48)&&(b2 <= 57)) b2 -= 48;
-            iaddr += (b1*16+b2);
-        }
-        return address(iaddr);
-    }
-
 }
 
